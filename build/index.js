d947f9053fe0e68e53907487f23f992113581be8
Boyuzhou <386607913@qq.com>
2016-11-21 11:03:54 +0800

    first commit

---
+++ build/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Fade = exports.Transition = undefined;
+
+var _Transition2 = require('./Transition');
+
+var _Transition3 = _interopRequireDefault(_Transition2);
+
+var _Fade2 = require('./Fade');
+
+var _Fade3 = _interopRequireDefault(_Fade2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports.Transition = _Transition3["default"];
+exports.Fade = _Fade3["default"];
